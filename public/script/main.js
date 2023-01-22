6c7ba4b5ec17dc7bb702eb1f3ede09de5fb2da19
xclusive-pc <ya.5164646777752015@yandex.ru>
2023-01-22 22:08:27 +0200

    Disable upload btn on submit

--- public/script/main.js
+++ public/script/main.js
@@ -1,3 +1,9 @@
 setTimeout(function () {
     $('.alert-success, .alert-info').alert('close');
 }, 2500);
+
+function disableButton() {
+    var btn = document.getElementById('upload-media-btn');
+    btn.disabled = true;
+    btn.innerText = 'Качва се...'
+}
